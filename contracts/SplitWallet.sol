6b904a6a5a9ae98e90bb0466802bd3ad7daa08a7
Rooh Afza <96720500+r0ohafza@users.noreply.github.com>
2024-02-28 15:56:45 -0800

    feat: blast support (#14)
    * feat: blast support
    
    * remove erc20 rebasing from wallet
    
    * address comment

--- contracts/SplitWallet.sol
+++ contracts/SplitWallet.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.4;
 
+import {IBlast} from 'contracts/interfaces/IBlast.sol';
 import {ISplitMain} from './interfaces/ISplitMain.sol';
 import {ERC20} from '@rari-capital/solmate/src/tokens/ERC20.sol';
 import {SafeTransferLib} from '@rari-capital/solmate/src/utils/SafeTransferLib.sol';
@@ -43,6 +44,12 @@ contract SplitWallet {
   /// @notice address of SplitMain for split distributions & EOA/SC withdrawals
   ISplitMain public immutable splitMain;
 
+  /// @notice blast yield contract.
+  IBlast public constant BLAST_YIELD = IBlast(0x4300000000000000000000000000000000000002);
+
+  /// @notice blast gas fees recipient.
+  address public immutable SPLITS_DEPLOYER;
+
   /**
    * MODIFIERS
    */
@@ -59,11 +66,17 @@ contract SplitWallet {
 
   constructor() {
     splitMain = ISplitMain(msg.sender);
+    SPLITS_DEPLOYER = tx.origin;
   }
 
   /**
    * FUNCTIONS - PUBLIC & EXTERNAL
    */
+
+  function initialize() external onlySplitMain {
+    BLAST_YIELD.configureAutomaticYield();
+    BLAST_YIELD.configureClaimableGas();
+  }
 
   /** @notice Sends amount `amount` of ETH in proxy to SplitMain
    *  @dev payable reduces gas cost; no vulnerability to accidentally lock
@@ -86,5 +99,10 @@ contract SplitWallet {
     onlySplitMain
   {
     token.safeTransfer(address(splitMain), amount);
+  }
+
+  /// @notice claim gas fees earned by this wallet and send it to split deployer.
+  function claimGasFees() external {
+    BLAST_YIELD.claimMaxGas(address(this), SPLITS_DEPLOYER);
   }
 }
